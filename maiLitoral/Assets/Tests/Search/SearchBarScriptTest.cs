bee3f277bedde4c41c5d538013ef95e4473ea8ab
Flakon <mail.david2004@gmail.com>
2026-04-26 18:53:37 +0300

    Added Tests for the search bar.

---
+++ maiLitoral/Assets/Tests/Search/SearchBarScriptTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using UnityEngine;
+using TMPro;
+
+public class SearchBarScriptTest
+{
+    [Test]
+    public void SearchBeach_WithEmptyInput_ShouldNotSaveSearchText()
+    {
+        BeachSearchData.searchText = "old value";
+
+        GameObject obj = new GameObject();
+        SearchBarScript script = obj.AddComponent<SearchBarScript>();
+
+        GameObject inputObj = new GameObject();
+        TMP_InputField input = inputObj.AddComponent<TMP_InputField>();
+
+        input.text = "";
+        script.nameInput = input;
+
+        script.SearchBeach();
+
+        Assert.AreEqual("old value", BeachSearchData.searchText);
+    }
+}
